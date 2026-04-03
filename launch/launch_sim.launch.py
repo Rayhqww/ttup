5bbc797ea9b704b083237f0b6286528d8ce025ef
Rayhqww <kirilldmitriev123@gmail.com>
2026-04-03 20:56:06 +0300

    Working gazebo simulation with diff drive and line world

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,8 +1,7 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 import xacro
@@ -11,11 +10,9 @@ def generate_launch_description():
     pkg_path = get_package_share_directory('ttup')
     world_file = os.path.join(pkg_path, 'worlds', 'line_world.world')
     
-    # Process xacro
     xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_raw = xacro.process_file(xacro_file).toxml()
     
-    # Robot State Publisher
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -23,55 +20,21 @@ def generate_launch_description():
         parameters=[{'robot_description': robot_description_raw, 'use_sim_time': True}]
     )
     
-    # Gazebo
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
         launch_arguments={'world': world_file, 'use_sim_time': 'true'}.items()
     )
     
-    # Spawn robot
     spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', '/robot_description', '-entity', 'ttup_robot'],
+        arguments=['-topic', '/robot_description', '-entity', 'ttup_robot', '-z', '0.1'],
         output='screen'
-    )
-    
-    # Controllers - start after spawn
-    diff_drive_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_cont", "--controller-manager", "/controller_manager"],
-        output="screen"
-    )
-    
-    joint_broad_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_broad", "--controller-manager", "/controller_manager"],
-        output="screen"
-    )
-    
-    # Delay controllers until after spawn
-    delay_diff_drive = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[diff_drive_spawner],
-        )
-    )
-    
-    delay_joint_broad = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[joint_broad_spawner],
-        )
     )
 
     return LaunchDescription([
         node_robot_state_publisher,
         gazebo,
         spawn_entity,
-        delay_diff_drive,
-        delay_joint_broad,
     ])
